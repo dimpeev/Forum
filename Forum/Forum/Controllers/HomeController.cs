83338ed1265986d8399cce2483e22352fc95a8d9
dimpeev <dimpeev@gmail.com>
2016-12-20 13:20:01 +0200

    Added search functionality. User can search by title, content, or by tag.

--- Forum/Forum/Controllers/HomeController.cs
+++ Forum/Forum/Controllers/HomeController.cs
@@ -31,11 +31,54 @@ namespace Forum.Controllers
             return View();
         }
 
+        // GET: HOME/Search
+        [HttpGet]
+        public ActionResult Search()
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: HOME/Search
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Search(string search)
-        { 
-            return View();
+        public ActionResult Search(string search, bool title = false, bool content = false, bool tag = false)
+        {
+
+            var searchWords = search.ToLower().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Distinct().ToList();
+
+            var articles = new List<Article>();
+
+            if (tag)
+            {
+                var searchResult = db.Articles.Where(a => a.Tags.Any(t => searchWords.Contains(t.Name.ToLower()))).ToList();
+                articles.AddRange(searchResult);
+            }
+
+            if (title)
+            {
+                var searchResult = db.Articles.Where(a => searchWords.Any(word => a.Title.ToLower().Contains(word))).ToList();
+                articles.AddRange(searchResult);
+            }
+
+            if (content)
+            {
+                var searchResult = db.Articles.Where(a => searchWords.Any(word => a.Content.ToLower().Contains(word))).ToList();
+                articles.AddRange(searchResult);
+
+                searchResult = db.Answers.Where(a => searchWords.Any(word => a.Content.ToLower().Contains(word))).Select(s => s.Article).ToList();
+                articles.AddRange(searchResult);
+
+            }
+
+            articles = articles.Distinct().ToList();
+
+            ViewBag.SearchString = search;
+            ViewBag.TitleCheckBox = Convert.ToBoolean(title);
+            ViewBag.ContentCheckBox = Convert.ToBoolean(content);
+            ViewBag.TagsCheckBox = Convert.ToBoolean(tag);
+
+            return View(articles);
         }
 
         protected override void Dispose(bool disposing)
